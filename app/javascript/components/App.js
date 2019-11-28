b066e66eb446839b73ccdc8b609e01158bd5ab2d
Belinda Lei <belinda.lei95@gmail.com>
2019-11-28 14:05:54 -0800

    removed redux'

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,29 +1,24 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Provider } from "react-redux";
 import Game from "./Game";
 import HomePage from './HomePage';
 import AboutPage from './AboutPage'
-import configureStore from "../configureStore";
-const store = configureStore();
 
 class App extends React.Component {
   render() {
     return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" render={() => <HomePage />} />
-            <Route
-              path="/game"
-              render={() => (
-                <Game />
-              )}
-            />
-            <Route path="/about" render={() => <AboutPage />} />
-          </Switch>
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => <HomePage />} />
+          <Route
+            path="/game"
+            render={() => (
+              <Game />
+            )}
+          />
+          <Route path="/about" render={() => <AboutPage />} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
